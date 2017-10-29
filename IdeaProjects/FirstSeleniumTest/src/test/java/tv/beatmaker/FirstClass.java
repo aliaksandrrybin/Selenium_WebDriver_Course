4422e952baea5d7bc2d4607259434de49c3fc562
aliaksandrrybin <aleks.rybin4@yandex.ru>
2017-10-29 18:09:28 +0300

    Задание 1

---
+++ IdeaProjects/FirstSeleniumTest/src/test/java/tv/beatmaker/FirstClass.java
@@ -0,0 +1,20 @@
+package tv.beatmaker;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class FirstClass {
+
+    @Test
+    void firstTest()
+    {
+        System.setProperty("webdriver.chrome.driver", "/Users/Asus/Documents/GitHub/chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+    driver.get("https://beatmaker.tv/");
+    String title = driver.getTitle();
+    Assert.assertTrue(title.equals("Основная площадка продажи/покупки битов в СНГ"));
+    driver.quit();
+    }
+
+}
